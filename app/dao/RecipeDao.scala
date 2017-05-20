4693d60fc767f941f88d980b429cf28fd5a2fb36
Jeffrey <jvbijleveld@gmail.com>
2017-05-20 16:41:10 +0200

    doodling

---
+++ app/dao/RecipeDao.scala
@@ -0,0 +1,17 @@
+package dao
+
+import models.Recipe
+import play.db.DBApi
+
+
+object RecipeDAo {
+  
+  def create(recipe : Recipe) = {
+  
+    
+    
+    
+  }
+  
+  
+}
--- app/services/RecipeService.java
+++ app/services/RecipeService.java
@@ -1,5 +1,14 @@
 package services;
 
+import java.util.List;
+
+import models.Recipe;
+
 public class RecipeService {
+	
+	public List<Recipe> getAllRecipes(){
+		List<Recipe> = Recipe.find.all();
+	}
+	
 
 }
